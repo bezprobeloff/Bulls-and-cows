10e594747434a177b5ea5932ef25a87cecb7177b
bezprobeloff <44836223+bezprobeloff@users.noreply.github.com>
2020-02-26 15:35:35 +0300

    Реализовано количество попыток угадать число

--- Bulls-and-cows.js
+++ Bulls-and-cows.js
@@ -1,30 +1,45 @@
 let numberUser;
 let numberSecret = `538902`;
+const countMoves = 3;
 const countDigits = numberSecret.toString().length;
 
-const readlineSync = require(`readline-sync`);
-numberUser = readlineSync.question(`Enter a number (`
-  + countDigits + ` digits): `);
-if (numberUser === numberSecret) {
-  console.log(`Вы выиграли.`);
-} else if (countDigits === numberUser.toString().length) {
-  let arrDigitsInPlace = Array.from(numberUser).filter(function(item, index) {
-    if (item === numberSecret[index]) {
-      return item;
+function play() {
+  const readlineSync = require(`readline-sync`);
+  let countMovesPlayed = countMoves;
+  while(countMovesPlayed > 0) {
+    numberUser = readlineSync.question(`Enter a number (`
+    + countDigits + ` digits): `);
+
+    if (numberUser === numberSecret) {
+      console.log(`Вы выиграли.`);
+      return;
+    } else if (countDigits === numberUser.toString().length) {
+      let arrDigitsInPlace = Array.from(numberUser).filter(function(item, index) {
+        if (item === numberSecret[index]) {
+          return item;
+        }
+      });
+
+      let arrDigitsNotInPlace = Array.from(numberUser).filter(function(item, index) {
+        if ((item !== numberSecret[index]) && (numberSecret.includes(item))) {
+          return item;
+        }
+      });
+
+      console.log(`Cовпавших цифр не на своих местах - `
+        + `${arrDigitsNotInPlace.length} (${arrDigitsNotInPlace}), ` +
+        `цифр на своих местах - ${arrDigitsInPlace.length} (${arrDigitsInPlace})`);
+        countMovesPlayed--;
+      if(countMovesPlayed > 0)
+        console.log(`У вас осталось ${countMovesPlayed} попыток.`);
+      else
+        console.log(`Вы проиграли.`);
+    } else {
+      console.log(`Ошибка. Введите корректное число (`
+        + countDigits + ` значное)`);
     }
-  });
+  }
 
-  let arrDigitsNotInPlace = Array.from(numberUser).filter(function(item, index) {
-    if ((item !== numberSecret[index]) && (numberSecret.includes(item))) {
-      return item;
-    }
-  });
-
-  console.log(`Cовпавших цифр не на своих местах -
-    ${arrDigitsNotInPlace.length} (${arrDigitsNotInPlace}), ` +
-    `цифр на своих местах - ${arrDigitsInPlace.length} (${arrDigitsInPlace})`);
-
-} else {
-  console.log(`Ошибка. Введите корректное число (`
-    + countDigits + ` значное)`);
 }
+
+play();

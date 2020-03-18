122a704195731660a2ca5169f9cb0aef63305834
bezprobeloff <bezprobeloff@gmail.com>
2020-03-18 22:42:57 +0300

    Реализован функционал. Корректно проходит тесты. Добавлен структура сценария

--- steps/tictactoe.js
+++ steps/tictactoe.js
@@ -2,7 +2,7 @@ const defineSupportCode = require('cucumber').defineSupportCode;
 const expect = require('chai').expect;
 
 function writeBox(x, y, player, box) {
-  const move = box.split('|').map((itemY, indexY) => {
+  const move = box.split('/').map((itemY, indexY) => {
     if (indexY === (Number(y) - 1)) {
       // eslint-disable-next-line no-param-reassign
       itemY = itemY.split('').map((itemX, indexX) => {
@@ -14,13 +14,13 @@ function writeBox(x, y, player, box) {
         .join('');
     }
     return itemY;
-  }).join('|');
+  }).join('/');
 
   return move;
 }
 
 function correctMove(x, y, box) {
-  const correct = box.split('|')
+  const correct = box.split('/')
     .filter((item, index) => index === Number(y) - 1)
     .join('')
     .split('')
@@ -32,34 +32,46 @@ function correctMove(x, y, box) {
   return true;
 }
 
-function getWinner(player, box) {
-  const arrBox = box.split('|');
+function getWinner(box) {
+  const arrBox = box.split('/');
   // создаем массив переводим вертикальные строки в горизонтальные
   const arrVertical = ['', '', ''];
+  const arrDiagonal = ['', ''];
   arrBox.forEach((item) => {
     item.split('').forEach((char, index) => {
       arrVertical[index] += char;
     });
   });
+
+  arrBox.forEach((item, index) => {
+    item.split('').forEach((char, indexChar) => {
+      if (index === indexChar) arrDiagonal[0] += char;
+      if (indexChar === (item.length - 1) - index) arrDiagonal[1] += char;
+    });
+  });
+
   const checkHorizontal = arrBox
     .filter((item) => item.match(/(.)\1{2}/i) !== null);
 
   const checkVertical = arrVertical
     .filter((item) => item.match(/(.)\1{2}/i) !== null);
 
+  const checkDiagonal = arrDiagonal
+    .filter((item) => item.match(/(.)\1{2}/i) !== null);
 
+  if (checkHorizontal.length > 0) return checkHorizontal[0].match(/./i)[0];
+  if (checkVertical.length > 0) return checkVertical[0].match(/./i)[0];
+  if (checkDiagonal.length > 0) return checkDiagonal[0].match(/./i)[0];
   return 0;
 }
 
-getWinner('1', '121|121|222');
-
-defineSupportCode(({ Given, Then, When }) => {
+defineSupportCode(({ Given, Then }) => {
   let box = '';
   let player;
   let status;
 
   Given('пустое поле', () => {
-    box = '000|000|000';
+    box = '000/000/000';
   });
 
   Given('поле {string}', (string) => {
@@ -90,9 +102,8 @@ defineSupportCode(({ Given, Then, When }) => {
     expect(status).to.false;
   });
 
-  Then('победил игрок {int}', (int) => {
-    // Then('победил игрок {float}', function (float) {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
+  Then('победил игрок {int}', (inter) => {
+    const test = Number(getWinner(box));
+    expect(test).equal(inter);
+  });
 });
